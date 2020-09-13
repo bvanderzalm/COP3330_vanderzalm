8d41652e0e9d25c39927f8a6a6e229d654347c32
Bradley Vanderzalm <bvan544@gmail.com>
2020-09-13 00:00:24 -0400

    Added function that changes the elements of an array by adding 7 and taking the remainder after dividing by 10

--- vanderzalm_p1/src/Encrypter.java
+++ vanderzalm_p1/src/Encrypter.java
@@ -2,36 +2,57 @@ import java.util.Arrays;
 
 public class Encrypter
 {
-    public String encrypt(String stringInput)
-    {
-        int [] fourDigitArray = StringToSequenceofIntegers(stringInput);
+    public String encrypt(String stringInput) {
+        int [] unencryptedArray = stringToArrayofIntegers(stringInput);
 
-        
+        int [] tempArray = plusSevenRemainder(unencryptedArray);
 
+        System.out.println(Arrays.toString(tempArray));
 
+//        int [] encryptedArray = swapDigits(tempArray);
 
+//        String encryptedOutput = convertArrayToString(encryptedArray);
 
-
-
-
-
-
-
-
-
-        String encryptedOutput = new String("0189");
-        return encryptedOutput;
+        return "0189";
     }
 
-    public int [] StringToSequenceofIntegers(String stringInput)
-    {
+    public int [] stringToArrayofIntegers(String stringInput) {
         int size = stringInput.length();
-        int [] fourDigitArray = new int [size];
+        int [] unencryptedArray = new int [size];
 
         for (int i = 0; i < size; i++)
-            fourDigitArray[i] = stringInput.charAt(i) - '0';
+            unencryptedArray[i] = stringInput.charAt(i) - '0';
 
-        return fourDigitArray;
+        return unencryptedArray;
     }
+
+    public int [] plusSevenRemainder(int [] array) {
+        int size = array.length;
+//        int [] newArray = new int [size];
+//        System.arraycopy(array, 0, newArray, 0, size);
+        for (int i = 0; i < size; i++) {
+            if (array[i] <= 2) {
+                array[i] += 7;
+            }
+            else {
+                array[i] += 7;
+                array[i] = (array[i] % 10);
+            }
+        }
+        return array;
+    }
+
+//    public int [] swapDigits(int [] tempArray)
+//    {
+//
+//
+//        return encryptedArray;
+//    }
+//
+//    public String convertArrayToString(int [] encryptedArray)
+//    {
+//
+//        return encryptedString;
+//    }
 
 }
