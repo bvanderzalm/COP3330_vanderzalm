d68017350337cdc12888f17d75377d4fff963f19
Bradley Vanderzalm <bvan544@gmail.com>
2020-09-12 20:22:29 -0400

    Added function to translate string into int array

--- vanderzalm_p1/src/Encrypter.java
+++ vanderzalm_p1/src/Encrypter.java
@@ -1,2 +1,37 @@
-public class Encrypter {
+import java.util.Arrays;
+
+public class Encrypter
+{
+    public String encrypt(String stringInput)
+    {
+        int [] fourDigitArray = StringToSequenceofIntegers(stringInput);
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+        String encryptedOutput = new String("0189");
+        return encryptedOutput;
+    }
+
+    public int [] StringToSequenceofIntegers(String stringInput)
+    {
+        int size = stringInput.length();
+        int [] fourDigitArray = new int [size];
+
+        for (int i = 0; i < size; i++)
+            fourDigitArray[i] = stringInput.charAt(i) - '0';
+
+        return fourDigitArray;
+    }
+
 }
