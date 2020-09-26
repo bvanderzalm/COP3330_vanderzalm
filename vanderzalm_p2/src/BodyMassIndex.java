1a895c0504daa346f06aa10235cf09817f54d309
Bradley Vanderzalm <bvan544@gmail.com>
2020-09-26 16:20:20 -0400

    Initial Commit for BodyMassIndex and ArrayList classes

---
+++ vanderzalm_p2/src/BodyMassIndex.java
@@ -0,0 +1,7 @@
+public class BodyMassIndex {
+
+    public BodyMassIndex(double height, double weight) {
+        System.out.println("Height: "+height);
+        System.out.println("Weight: "+weight);
+    }
+}
