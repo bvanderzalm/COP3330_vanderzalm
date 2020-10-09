641c495145767d2986421a38af13dfc8d24c87c6
Bradley Vanderzalm <bvan544@gmail.com>
2020-10-09 16:30:34 -0400

    Initial Commit for super and subclasses. Abstraction and inheritance was tested successfully.

---
+++ vanderzalm_p3/src/Shape.java
@@ -0,0 +1,41 @@
+public abstract class Shape {
+
+    public Shape() {}
+
+    public abstract String getName();
+
+    public abstract double getArea();
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ vanderzalm_p3/src/Shape2D.java
@@ -0,0 +1,10 @@
+public abstract class Shape2D extends Shape{
+
+    public Shape2D() {
+        super();
+    }
+
+    public abstract String getName();
+
+    public abstract double getArea();
+}
