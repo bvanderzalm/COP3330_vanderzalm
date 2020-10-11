aa100b851f05f6803f45327f3c909bb50b808848
Bradley Vanderzalm <bvan544@gmail.com>
2020-10-10 21:45:02 -0400

    Initial commit for 3D shapes. Successfully tested getVolume in Cube class.

---
+++ vanderzalm_p3/src/Cube.java
@@ -0,0 +1,25 @@
+public class Cube extends Shape3D {
+
+    private double edge;
+
+    public Cube(double edge) {
+        super();
+        this.edge = edge;
+    }
+
+    public String getName() {
+        return "cube";
+    }
+
+    public double getArea() {
+        return 6 * edge * edge;
+    }
+
+    public double getVolume() {
+        return edge * edge * edge;
+    }
+
+
+
+
+}
