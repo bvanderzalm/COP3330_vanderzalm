159461616623e41935506de4bc37da7e94129b6e
Bradley Vanderzalm <bvan544@gmail.com>
2020-10-10 21:30:20 -0400

    Initial Commit for circle. Imported Math class.

---
+++ vanderzalm_p3/src/Circle.java
@@ -0,0 +1,19 @@
+import java.lang.Math;
+
+public class Circle extends Shape2D {
+
+    private final double radius;
+
+    public Circle(double radius) {
+        super();
+        this.radius = radius;
+    }
+
+    public String getName() {
+        return "circle";
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
