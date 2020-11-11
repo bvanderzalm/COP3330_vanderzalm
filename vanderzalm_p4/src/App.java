8e49fb72d130dc92ffe8c3b5e25f9d0d2a33b3ad
Bradley Vanderzalm <bvan544@gmail.com>
2020-11-11 02:48:21 -0500

    Set up menu for list operation to handle exceptions and be able to quit back to the main menu if need be.

--- vanderzalm_p4/src/App.java
+++ vanderzalm_p4/src/App.java
@@ -15,11 +15,11 @@ public class App {
         do {
             try {
                 displayMainMenuOptions();
-                int userInput = scnr.nextInt();
-                continueLoop = processUserInput(userInput);
+                int mainMenuUserInput = scnr.nextInt();
+                continueLoop = processMainMenuUserInput(mainMenuUserInput);
             } catch (InputMismatchException ex) {
                 scnr.nextLine();
-                System.out.print("Invalid input, please only enter the numbers listed above (1, 2, 3)\n\n");
+                System.out.print("Invalid input, only enter the numbers listed above (1, 2, 3).\n\n");
             } catch (Exception ex) {
                 scnr.nextLine();
                 System.out.print("Unexpected error, please try again.\n\n");
@@ -35,30 +35,86 @@ public class App {
         System.out.print("> ");
     }
 
-    public boolean processUserInput(int userInput) {
+    public boolean processMainMenuUserInput(int userInput) {
         boolean continueLoop = true;
-        if (inputInRange(userInput)) {
-            if (readyToExit(userInput))
+        if (mainMenuInputValid(userInput)) {
+            if (readyToExitProgram(userInput))
                 continueLoop = false;
             else
-                listOperationMenu(userInput);
+                createOrLoadList(userInput);
         }
         else System.out.print("That menu option doesn't exist. Please enter 1, 2, or 3.\n\n");
         return continueLoop;
     }
 
-    public boolean inputInRange(int input) {
-        return (input >= 1 && input <= 3);
+    public boolean mainMenuInputValid(int userInput) {
+        return (userInput >= 1 && userInput <= 3);
     }
 
-    public boolean readyToExit(int userInput) {
+    public boolean readyToExitProgram(int userInput) {
         return (userInput == 3);
     }
 
-    public void listOperationMenu(int userInput) {
-        // Doesn't do anything yet but should create a new list or load a list based on userInput
-        System.out.println("List Operation Menu");
+    public void createOrLoadList(int userInput) {
+        if (userInput == 1) {
+            System.out.print("New task list has been created\n\n");
+            listOperationMenu();
+        } else
+            System.out.print("Load list\n");
     }
 
+    public void listOperationMenu() {
+        boolean continueLoop = true;
+        do {
+            try {
+                displayTaskMenuOptions();
+                int taskMenuUserInput = scnr.nextInt();
+                continueLoop = processTaskMenuUserInput(taskMenuUserInput);
+            } catch (InputMismatchException ex) {
+                scnr.nextLine();
+                System.out.print("Invalid input, only enter the numbers listed above (1-8).\n\n");
+            } catch (Exception ex) {
+                scnr.nextLine();
+                System.out.print("Unexpected error, please try again.\n\n");
+            }
+        } while(continueLoop);
+    }
 
+    public void displayTaskMenuOptions() {
+        System.out.print("List Operation Menu\n---------\n\n");
+        System.out.println("1) View the list");
+        System.out.println("2) Add an item");
+        System.out.println("3) Edit an item");
+        System.out.println("4) Remove an item");
+        System.out.println("5) Mark an item as completed");
+        System.out.println("6) Unmark an item as completed");
+        System.out.println("7) Save the current list");
+        System.out.println("8) Quit to the main menu");
+        System.out.print("\n> ");
+    }
+
+    public boolean processTaskMenuUserInput(int userInput) {
+        boolean continueLoop = true;
+        if (taskMenuInputValid(userInput)) {
+            if (readyToQuitTaskMenu(userInput))
+                continueLoop = false;
+            else
+                taskMenuOptions(userInput);
+        }
+        else
+            System.out.print("That menu option doesn't exist. Please enter a number 1 through 8.\n\n");
+        return continueLoop;
+    }
+
+    public boolean taskMenuInputValid(int userInput) {
+        return (userInput >= 1 && userInput <= 8);
+    }
+
+    public boolean readyToQuitTaskMenu(int userInput) {
+        return (userInput == 8);
+    }
+
+    public void taskMenuOptions(int userInput) {
+        System.out.println("Doing the things listed in items 1 - 8 based on your choice...");
+    }
 }
