adf38dd3069bfc6b333dd0ecd3b95ef8e6b505e0
Bradley Vanderzalm <bvan544@gmail.com>
2020-11-12 22:43:22 -0500

    Initial commit for TaskItem. Constructor takes input, checks if title and due Date are valid and throws newly created exceptions seen at the bottom.

--- vanderzalm_p4/src/TaskItem.java
+++ vanderzalm_p4/src/TaskItem.java
@@ -1,2 +1,46 @@
+import java.time.LocalDate;
+
 public class TaskItem {
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private boolean completed;
+
+    public TaskItem(String title, String description, LocalDate dueDate, boolean completed) {
+
+        if (isTitleValid(title)) {
+            this.title = title;
+        } else {
+            throw new InvalidTitleException("Title is not valid; must be at least 1 character long.");
+        }
+
+        if (isDueDateValid(dueDate)) {
+            this.dueDate = dueDate;
+            this.description = description;
+            this.completed = false;
+        } else {
+            throw new InvalidDueDateException("Due date is not valid; cannot be before the current date.");
+        }
+    }
+
+    private boolean isTitleValid(String title) {
+        return (title.length() > 0);
+    }
+
+    private boolean isDueDateValid(LocalDate dueDate) {
+        LocalDate currentDate = LocalDate.now();
+        return dueDate.isAfter(currentDate);
+    }
+}
+
+class InvalidTitleException extends IllegalArgumentException {
+    public InvalidTitleException(String msg) {
+        super(msg);
+    }
+}
+
+class InvalidDueDateException extends IllegalArgumentException {
+    public InvalidDueDateException(String msg) {
+        super(msg);
+    }
 }
