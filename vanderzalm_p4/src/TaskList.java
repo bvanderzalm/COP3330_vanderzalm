42c7ad93d3cf139876270154ab63ec74c5666db6
Bradley Vanderzalm <bvan544@gmail.com>
2020-11-13 00:09:03 -0500

    Updated TaskList to add Task objects into an ArrayList. Also added a few other helper methods used to print the current task in App class.

--- vanderzalm_p4/src/TaskList.java
+++ vanderzalm_p4/src/TaskList.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskList {
+    List<TaskItem> tasks;
+
+    public TaskList() {
+        tasks = new ArrayList<>();
+    }
+
+    public void add(TaskItem items) {
+        tasks.add(items);
+    }
+
+    public int getSize() {
+        return tasks.size();
+    }
+
+    public TaskItem get(int index) {
+        return tasks.get(index);
+    }
+
+
+
+
+
 }
