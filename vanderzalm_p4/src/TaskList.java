0a0b33cffc309ad8173e681d57823be2f42ea371
Bradley Vanderzalm <bvan544@gmail.com>
2020-11-14 23:55:57 -0500

    Created a load method which reads data from a text file creating in write, creates a new TaskItem and adds it to TaskList. Also adjusted the write method to store the task items one line at a time. This is because the load method struggled to store the string unless instructed to read the entire line <input.nextLine()>

--- vanderzalm_p4/src/TaskList.java
+++ vanderzalm_p4/src/TaskList.java
@@ -1,7 +1,12 @@
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Formatter;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class TaskList {
     List<TaskItem> tasks;
@@ -10,8 +15,8 @@ public class TaskList {
         tasks = new ArrayList<>();
     }
 
-    public void add(TaskItem items) {
-        tasks.add(items);
+    public void add(TaskItem task) {
+        tasks.add(task);
     }
 
     public int getSize() {
@@ -40,21 +45,46 @@ public class TaskList {
             for (int i = 0; i < tasks.size(); i++) {
                 TaskItem task = tasks.get(i);
                 if (task.getCompletedStatus() == true) {
-                    completedStatus = "completed";
+                    completedStatus = "t";
                 } else {
-                    completedStatus = "uncompleted";
+                    completedStatus = "f";
                 }
-                output.format("%s: %s [%s] - %s%n",
+                output.format("%s%n%s%n%s%n%s%n",
                         task.getTitle(), task.getDescription(), task.getDueDateString(), completedStatus);
-
-
             }
             System.out.println("Task list has been saved as " + filename);
 
         } catch (FileNotFoundException ex) {
             System.out.println("Error, unable to find the file.");
+        } catch (NoSuchElementException | IllegalStateException ex) {
+            System.out.println("Error attempting to save " + filename);
         } catch (Exception ex) {
             System.out.println("Unexpected error occurred while attempting to save file. Please try again.");
         }
+    }
+
+    public void load(String filename) {
+        try (Scanner input = new Scanner(Paths.get(filename))) {
+            while (input.hasNext()) {
+                String title = input.nextLine();
+                String description = input.nextLine();
+                String dueDateString = input.nextLine();
+                String completedStatusString = input.nextLine();
+
+                LocalDate dueDate = LocalDate.parse(dueDateString);
+                boolean completedStatus = convertToBoolean(completedStatusString);
+                TaskItem task = new TaskItem(title, description, dueDate, completedStatus);
+                tasks.add(task);
+            }
+            System.out.println("Task list has been loaded from " + filename + "\n");
+        } catch (NoSuchElementException | IllegalStateException | IOException ex) {
+            System.out.println("Error occurred after attempting to load " + filename);
+        } catch (Exception ex) {
+            System.out.println("An unexpected error occurred while attempting to load this file.");
+        }
+    }
+
+    private boolean convertToBoolean(String str) {
+        return str.startsWith("t");
     }
 }
