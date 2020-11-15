9fad6a0fb98aa092f3368a6b6cde499b23eed49f
Bradley Vanderzalm <bvan544@gmail.com>
2020-11-15 00:30:23 -0500

    Added JUnit tests to ensure failure or success when setting new Titles and Due Dates that are either invalid or valid. Tested successfully.

--- vanderzalm_p4/src/TaskItemTest.java
+++ vanderzalm_p4/src/TaskItemTest.java
@@ -8,8 +8,8 @@ class TaskItemTest {
 
     // NOTE: TaskItem holds a LocalDate variable and this converts it (from a string)
     // Used throughout the test cases.
-    LocalDate invalidDate = LocalDate.parse("2000-01-01");
-    LocalDate validDate = LocalDate.parse("3000-01-01");
+    private final LocalDate invalidDate = LocalDate.parse("2000-01-01");
+    private final LocalDate validDate = LocalDate.parse("3000-01-01");
 
     @Test
     public void creatingTaskItemFailsWithInvalidDueDate() {
@@ -32,4 +32,35 @@ class TaskItemTest {
         TaskItem t = new TaskItem("Task1", "", validDate, false);
         assertEquals("Task1", t.getTitle());
     }
+
+    @Test
+    public void settingTaskItemDueDateFailsWithInvalidDate() {
+        // Create task with a VALID date first, then set it with an INVALID date.
+        TaskItem t = new TaskItem("Task1", "", validDate, false);
+        assertThrows(InvalidDueDateException.class, () -> t.setDueDate(invalidDate));
+    }
+
+    @Test
+    public void settingTaskItemDueDateSucceedsWithValidDate() {
+        TaskItem t = new TaskItem("Task1", "", validDate, false);
+        LocalDate secondDueDate = LocalDate.parse("2100-12-12");
+        t.setDueDate(secondDueDate);
+
+        assertEquals(secondDueDate, t.getDueDate());
+    }
+
+    @Test
+    public void settingTaskItemTitleFailsWithInvalidTitle() {
+        TaskItem t = new TaskItem("Initial valid title", "", validDate, false);
+        assertThrows(InvalidTitleException.class, () -> t.setTitle(""));
+    }
+
+    @Test
+    public void settingTaskItemTitleSucceedsWithValidTitle() {
+        TaskItem t = new TaskItem("Initial valid title", "", validDate, false);
+        t.setTitle("Valid Task title");
+
+        assertEquals("Valid Task title", t.getTitle());
+    }
+
 }
