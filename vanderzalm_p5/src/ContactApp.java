c8cd15cb28059fa52f7f6d519ff3e4f178d1f801
Bradley Vanderzalm <bvan544@gmail.com>
2020-11-22 18:46:37 -0500

    Created framework for both App classes to run through it's menu taking user input.

--- vanderzalm_p5/src/ContactApp.java
+++ vanderzalm_p5/src/ContactApp.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class ContactApp {
+
+    private static Scanner scnr = new Scanner(System.in);
+
+    private ContactList contacts;
+
+    public void run() {
+        System.out.println("Testing ContactApp");
+    }
 }
--- vanderzalm_p5/src/TaskApp.java
+++ vanderzalm_p5/src/TaskApp.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class TaskApp {
+
+    private static Scanner scnr = new Scanner(System.in);
+
+    private TaskList tasks;
+
+    public void run() {
+        System.out.println("Testing TaskList");
+    }
+
+
 }
