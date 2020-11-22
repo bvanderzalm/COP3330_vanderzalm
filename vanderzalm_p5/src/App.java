4a5c951c7c7213bd223a5bbe2b0eeef59a5de10e
Bradley Vanderzalm <bvan544@gmail.com>
2020-11-22 18:37:52 -0500

    Created method to take user input in the main menu and then proceeds to go to either ContactApp or TaskApp to run the next menu.

--- vanderzalm_p5/src/App.java
+++ vanderzalm_p5/src/App.java
@@ -1,2 +1,41 @@
+import java.util.Scanner;
+
 public class App {
+
+    private static Scanner scnr = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        App m = new App();
+        m.selectApplication();
+    }
+
+    public void selectApplication() {
+        int applicationChoice;
+        while(true) {
+            displayApplicationMenu();
+            applicationChoice = scnr.nextInt();
+
+            if (applicationChoice == 1) {
+                TaskApp t = new TaskApp();
+                t.run();
+            } else if (applicationChoice == 2) {
+                ContactApp c = new ContactApp();
+                c.run();
+            } else if (applicationChoice == 3) {
+                break;
+            } else {
+                System.out.println("Invalid input. Please try again.");
+            }
+        }
+    }
+
+    private void displayApplicationMenu() {
+        System.out.println("Select Your Application");
+        System.out.println("-----------------------\n");
+        System.out.println("1) Task List");
+        System.out.println("2) Contact List");
+        System.out.println("3) Quit\n");
+        System.out.print("> ");
+    }
+
 }
